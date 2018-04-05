f19b3b8af09776a5fe252dd5268b9fa64cb7a7a9
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2018-04-06 00:48:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const twitter = require('twitter');
+const client = new twitter(require('./token.json'));
+const fs = require('fs');
+const request = require('request');
+var array = [];
+
+var params = {
+    screen_name: '*****',
+    count : 200,
+    include_rts : false
+};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+        for (var i = 0; i < tweets.length; i++) {
+            var tweet = tweets[i];
+            if (tweet.extended_entities) {
+                for(var j = 0; j < tweet.extended_entities.media.length; j++) {
+                    console.log(tweet.extended_entities.media[j].media_url);
+                    array.push(tweet.extended_entities.media[j].media_url);
+                }
+            } else {
+                console.log("no image");
+            }
+        }
+        console.log(array);
+        for(var i = 0; i < array.length; i++){
+            request
+                .get(array[i])
+                .on('response', function (res) {
+                    console.log('statusCode: ', res.statusCode);
+                    console.log('content-length: ', res.headers['content-length']);
+                })
+                .pipe(fs.createWriteStream('./image/data' + [i] + '.jpg'));
+        }
+    }
+});
