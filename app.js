c20470aca6518a43dc37947832f0cd917f68c14d
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2018-04-15 00:19:52 +0900

    取得を3200tweetまで拡張

--- app.js
+++ app.js
@@ -7,31 +7,49 @@ const scanf = require('scanf');
 var id = scanf('%s');   //画像を採取したいアカウントのIDを入力
 var array = [];
 
+var MAX_LOOP = 16;
+var maxid;
+
 var params = {
     screen_name: id,
     count : 200,
+    max_id : maxid,
     include_rts : false
 };
 
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-    if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-            var tweet = tweets[i];
-            if (tweet.extended_entities) {
-                for(var j = 0; j < tweet.extended_entities.media.length; j++) {
-                    console.log(tweet.extended_entities.media[j].media_url);
-                    array.push(tweet.extended_entities.media[j].media_url);
-                }
-            }
-        }
+getImage(0);
+
+function getImage (loop) {
+    if (loop >= MAX_LOOP) {
+        console.log(array.length);
+
         for(var i = 0; i < array.length; i++){
             request
                 .get(array[i])
+                .on('error', function(err){
+                    console.log(err);
+                })
                 .on('response', function (res) {
                     console.log('statusCode: ', res.statusCode);
                     console.log('content-length: ', res.headers['content-length']);
                 })
                 .pipe(fs.createWriteStream('./image/data' + [i] + '.jpg')); //保存する場所を指定
         }
+        return;
     }
-});
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var tweet = tweets[i];
+                if (tweet.extended_entities) {
+                    for(var j = 0; j < tweet.extended_entities.media.length; j++) {
+                        console.log(tweet.extended_entities.media[j].media_url);
+                        array.push(tweet.extended_entities.media[j].media_url);
+                    }
+                }
+            }
+            params.max_id = tweet.id - 1; 
+            getImage(++loop);
+        }
+    });
+}
